48edda758239b9e530c77d7fd2ef54df4a76c76b
yuyo <yuyofritz99@gmail.com>
2025-09-19 01:32:35 -0300

    Sobrescribir remoto con versión local

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-    return (
-        <footer className="bg-dark text-white text-center py-3 mt-4">
-            <p>© 2025 - Pizzería Mamma Mia! - Todos los derechos reservados</p>
-        </footer>
-    );
-};
-
-export default Footer;
